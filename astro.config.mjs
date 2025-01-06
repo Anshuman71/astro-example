6fc16b179bb0b0cf1e09e0d08e18c73e65ec38d4
Anshuman Bhardwaj <anshu5074@gmail.com>
2025-01-06 17:56:27 +0530

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: netlify()
 });
